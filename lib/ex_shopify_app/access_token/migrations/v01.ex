0b11df60fb29cb56fc85c04cb0cfe71a6d1baab7
NexPB <1289189+NexPB@users.noreply.github.com>
2026-06-16 12:04:01 +0900

    chore: add :ecto as a formatter

--- lib/ex_shopify_app/access_token/migrations/v01.ex
+++ lib/ex_shopify_app/access_token/migrations/v01.ex
@@ -13,30 +13,30 @@ if Code.ensure_loaded?(Ecto.Migration) do
     """
     def up do
       create_if_not_exists table(:shopify_access_tokens, primary_key: false) do
-        add(:shopify_domain, :string, primary_key: true)
-        add(:access_token, :text, null: false)
-        add(:refresh_token, :text)
-        add(:scope, :text)
+        add :shopify_domain, :string, primary_key: true
+        add :access_token, :text, null: false
+        add :refresh_token, :text
+        add :scope, :text
 
-        add(:expires_in, :integer)
-        add(:expires_at, :utc_datetime)
-        add(:refresh_token_expires_in, :integer)
-        add(:refresh_token_expires_at, :utc_datetime)
+        add :expires_in, :integer
+        add :expires_at, :utc_datetime
+        add :refresh_token_expires_in, :integer
+        add :refresh_token_expires_at, :utc_datetime
 
-        add(:last_refreshed_at, :utc_datetime_usec)
-        add(:last_refresh_error, :text)
-        add(:refresh_generation, :integer, null: false, default: 0)
+        add :last_refreshed_at, :utc_datetime_usec
+        add :last_refresh_error, :text
+        add :refresh_generation, :integer, null: false, default: 0
 
         timestamps(type: :utc_datetime_usec)
       end
 
-      create_if_not_exists(index(:shopify_access_tokens, [:expires_at]))
-      create_if_not_exists(index(:shopify_access_tokens, [:refresh_token_expires_at]))
+      create_if_not_exists index(:shopify_access_tokens, [:expires_at])
+      create_if_not_exists index(:shopify_access_tokens, [:refresh_token_expires_at])
     end
 
     @doc "Drops the `shopify_access_tokens` table."
     def down do
-      drop_if_exists(table(:shopify_access_tokens))
+      drop_if_exists table(:shopify_access_tokens)
     end
   end
 end
